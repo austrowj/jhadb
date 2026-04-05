2bbd75661567c51b0b2fe7795dab963fa4d80481
austrowj <austrowj@users.noreply.github.com>
2026-04-05 12:43:54 -0400

    feat: refactor to try out tagged unions

--- src/root.zig
+++ src/root.zig
@@ -2,22 +2,35 @@
 const std = @import("std");
 const Io = std.Io;
 
+const Command = union(enum) {
+    quit: void,
+    echo_unrecognized: []const u8,
+};
+
 /// Read, evaluate, print, loop
 pub fn repl(reader: *Io.Reader, writer: *Io.Writer) !void {
     while (true) {
         _ = try writer.write("\n> ");
         try writer.flush();
         const line = try reader.takeDelimiter('\n') orelse unreachable;
-        const result = eval(line);
-        try writer.writeAll(result);
+
+        switch (eval(line)) {
+            .quit => return,
+            .echo_unrecognized => |msg| {
+                try writer.writeAll("Unrecognized command: '");
+                try writer.writeAll(msg);
+                try writer.writeAll("'\n");
+            },
+        }
     }
 }
 
-fn eval(buf: []const u8) []const u8 {
+fn eval(buf: []const u8) Command {
     if (std.mem.eql(u8, buf, "quit()")) {
-        std.process.exit(0);
+        return Command{ .quit = {} };
+    } else {
+        return Command{ .echo_unrecognized = buf };
     }
-    return buf;
 }
 
 pub fn add(a: i32, b: i32) i32 {
